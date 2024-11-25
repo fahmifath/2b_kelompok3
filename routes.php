fbb39bd51e670e010ff593959a6321b7cf977cac
fahmifath <fahmifthur@gmail.com>
2024-11-25 18:34:25 +0700

    edit route & testing

--- routes.php
+++ routes.php
@@ -3,25 +3,25 @@
 
 require_once 'app/controllers/MemberController.php';
 
-$controller = new MemberController();
+$memberController = new MemberController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($url == '/member/index' || $url == '/') {
-    $controller->index();
+    $memberController->index();
 } elseif ($url == '/member/create' && $requestMethod == 'GET') {
-    $controller->create();
+    $memberController->create();
 } elseif ($url == '/member/store' && $requestMethod == 'POST') {
-    $controller->store();
+    $memberController->store();
 } elseif (preg_match('/\/member\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->edit($userId);
+    $memberController->edit($userId);
 } elseif (preg_match('/\/member\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
     $userId = $matches[1];
-    $controller->update($userId, $_POST);
+    $memberController->update($userId, $_POST);
 } elseif (preg_match('/\/member\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->delete($userId);
+    $memberController->delete($userId);
 } else {
     http_response_code(404);
     echo "404 Not Found";
