35571acc1a09280e1f15e293773284db198ef634
AnayAilirpa <165096298+AnayAilirpa@users.noreply.github.com>
2024-11-25 14:11:24 +0700

    Update index.php

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -1,12 +1,14 @@
-<!-- app/views/user/index.php -->
-<h2>Daftar Pengguna</h2>
-<a href="/user/create">Tambah Pengguna Baru</a>
+<!-- app/views/kelas/index.php -->
+<h2>Daftar Kelas</h2>
+<a href="/kelas/create">Tambah Kelas Baru</a>
 <ul>
-    <?php foreach ($users as $user): ?>
+    <?php foreach ($kelas as $kelas): ?>
         <div>
-            <p><?= htmlspecialchars($user['name']) ?> - <?= htmlspecialchars($user['email']) ?>
-            <a href="/user/edit/<?php echo $user['id']; ?>">Edit</a> |
-            <a href="/user/delete/<?php echo $user['id']; ?>" onclick="return confirm('Are you sure?')">Delete</a>
+            
+            <p><?= htmlspecialchars($kelas['nama_kelas']) ?> - <?= htmlspecialchars($kelas['waktu']) ?> - <?= htmlspecialchars($kelas['peltih']) ?> - <?= htmlspecialchars($kelas['kuota']) ?>
+            
+            <a href="/kelas/edit/<?php echo $kelas['id_kelas']; ?>">Edit</a> |
+            <a href="/kelas/delete/<?php echo $kelas['id_kelas']; ?>" onclick="return confirm('Are you sure?')">Delete</a>
             </p>
         </div>
     <?php endforeach; ?>
