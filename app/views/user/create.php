8b21c32af4a32b0bdf6b01da5e712c16971a7005
tiaradinda020 <165237975+tiaradinda020@users.noreply.github.com>
2024-11-25 14:47:31 +0700

    Update create.php

--- app/views/user/create.php
+++ app/views/user/create.php
@@ -1,9 +1,11 @@
 <!-- app/views/user/create.php -->
 <h2>Tambah Pengguna Baru</h2>
 <form action="/user/store" method="POST">
-    <label for="name">Nama:</label>
+    <label for="name">Nama Alat:</label>
     <input type="text" name="name" id="name" required>
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" required>
+    <label for="type">Jenis Alat:</label>
+    <input type="type" name="type" id="type" required>
+    <label for="type">Kondisi:</label>
+    <input type="type" name="type" id="type" required>
     <button type="submit">Simpan</button>
 </form>
