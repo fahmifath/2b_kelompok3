f396e82bede4f429fcf1a2085577219c0d740def
AnayAilirpa <dianaaprilia745@gmail.com>
2024-11-26 13:17:51 +0700

    Update

--- app/controllers/WorkoutClassController.php
+++ app/controllers/WorkoutClassController.php
@@ -3,20 +3,20 @@
 require_once '../app/models/WorkoutClass.php';
 
 class WorkoutClassController {
-    private $kelasModel;
+    private $workoutClassModel;
 
     public function __construct() {
-        $this->kelasModel = new WorkoutClass();
+        $this->workoutClassModel = new WorkoutClass();
     }
 
     public function index() {
-        $kelas = $this->kelasModel->getAllWorkoutClass();
-        require_once '../app/views/WorkoutClass/index.php';
+        $kelas = $this->workoutClassModel->getAllWorkoutClass();
+        require_once '../app/views/workout_class/index.php';
 
     }
 
     public function create() {
-        require_once '../app/views/WorkoutClass/create.php';
+        require_once '../app/views/workout_class/create.php';
     }
 
     public function store() {
@@ -24,20 +24,20 @@ class WorkoutClassController {
         $waktu = $_POST['waktu'];
         $pelatih = $_POST['pelatih'];
         $kuota = $_POST['kuota'];
-        $this->kelasModel->add($nama_kelas, $waktu, $pelatih, $kuota);
-        header('Location: /WorkoutClass/index');
+        $this->workoutClassModel->add($nama_kelas, $waktu, $pelatih, $kuota);
+        header('Location: /workout_class/index');
     }
     // Show the edit form with the kelas data
     public function edit($id_kelas) {
-        $kelass = $this->kelasModel->find($id_kelas); // Assume find() gets kelas by ID
-        require_once __DIR__ . '/../views/WorkoutClass/edit.php';
+        $kelass = $this->workoutClassModel->find($id_kelas); // Assume find() gets kelas by ID
+        require_once __DIR__ . '/../views/workout_class/edit.php';
     }
 
     // Process the update request
     public function update($id_kelas, $data) {
-        $updated = $this->kelasModel->update($id_kelas, $data);
+        $updated = $this->workoutClassModel->update($id_kelas, $data);
         if ($updated) {
-            header("Location: /WorkoutClass/index"); // Redirect to kelas list
+            header("Location: /workout_class/index"); // Redirect to kelas list
         } else {
             echo "Failed to update kelas.";
         }
@@ -45,9 +45,9 @@ class WorkoutClassController {
 
     // Process delete request
     public function delete($id_kelas) {
-        $deleted = $this->kelasModel->delete($id_kelas);
+        $deleted = $this->workoutClassModel->delete($id_kelas);
         if ($deleted) {
-            header("Location: /WorkoutClass/index"); // Redirect to kelas list
+            header("Location: /workout_class/index"); // Redirect to kelas list
         } else {
             echo "Failed to delete kelas.";
         }
--- routes.php
+++ routes.php
@@ -1,51 +1,26 @@
 <?php
 // routes
-require_once 'app/controllers/MemberController.php';
 require_once 'app/controllers/WorkoutClassController.php';
-
-$memberController = new MemberController();
-$url = $_SERVER['REQUEST_URI'];
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-
-if ($url == '/member/index' || $url == '/') {
-    $memberController->index();
-} elseif ($url == '/member/create' && $requestMethod == 'GET') {
-    $memberController->create();
-} elseif ($url == '/member/store' && $requestMethod == 'POST') {
-    $memberController->store();
-} elseif (preg_match('/\/member\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $userId = $matches[1];
-    $memberController->edit($userId);
-} elseif (preg_match('/\/member\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
-    $userId = $matches[1];
-    $memberController->update($userId, $_POST);
-} elseif (preg_match('/\/member\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $userId = $matches[1];
-    $memberController->delete($userId);
-} else {
-    http_response_code(404);
-    echo "404 Not Found";
-}
 
 $workoutClassController = new WorkoutClassController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($url == '/WorkoutClass/index' || $url == '/') {
+if ($url == '/workout_class/index' || $url == '/') {
     $workoutClassController->index();
-} elseif ($url == '/WorkoutClass/create' && $requestMethod == 'GET') {
+} elseif ($url == '/workout_class/create' && $requestMethod == 'GET') {
     $workoutClassController->create();
-} elseif ($url == '/WorkoutClass/store' && $requestMethod == 'POST') {
+} elseif ($url == '/workout_class/store' && $requestMethod == 'POST') {
     $workoutClassController->store();
-} elseif (preg_match('/\/WorkoutClass\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $userId = $matches[1];
-    $workoutClassController->edit($userId);
-} elseif (preg_match('/\/WorkoutClass\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
-    $userId = $matches[1];
-    $workoutClassController->update($userId, $_POST);
-} elseif (preg_match('/\/WorkoutClass\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $userId = $matches[1];
-    $workoutClassController->delete($userId);
+} elseif (preg_match('/\/workout_class\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $id_kelas = $matches[1];
+    $workoutClassController->edit($id_kelas);
+} elseif (preg_match('/\/workout_class\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
+    $id_kelas = $matches[1];
+    $workoutClassController->update($id_kelas, $_POST);
+} elseif (preg_match('/\/workout_class\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $id_kelas = $matches[1];
+    $workoutClassController->delete($id_kelas);
 } else {
     http_response_code(404);
     echo "404 Not Found";
