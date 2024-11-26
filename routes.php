df644513b61ff141890b2158c4f2e35d69deb31a
tiaradinda020 <165237975+tiaradinda020@users.noreply.github.com>
2024-11-26 13:17:31 +0700

    update eqipmentController

--- routes.php
+++ routes.php
@@ -3,25 +3,25 @@
 
 require_once 'app/controllers/EquipmentController.php';
 
-$controller = new EquipmentController();
+$equipmentController = new EquipmentController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($url == '/equipment/index' || $url == '/') {
-    $controller->index();
+    $equipmentController->index();
 } elseif ($url == '/equipment/create' && $requestMethod == 'GET') {
-    $controller->create();
+    $equipmentController->create();
 } elseif ($url == '/equipment/store' && $requestMethod == 'POST') {
-    $controller->store();
+    $equipmentController->store();
 } elseif (preg_match('/\/equipment\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->edit($userId);
+    $equipmentController->edit($userId);
 } elseif (preg_match('/\/equipment\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
     $userId = $matches[1];
-    $controller->update($userId, $_POST);
+    $equipmentController->update($userId, $_POST);
 } elseif (preg_match('/\/equipment\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->delete($userId);
+    $equipmentController->delete($userId);
 } else {
     http_response_code(404);
     echo "404 Not Found";
