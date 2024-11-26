3de920e0e7cfff65c36d31ba420bf85ce6c266ad
AnayAilirpa <dianaaprilia745@gmail.com>
2024-11-26 09:01:35 +0700

    Pweb2

--- app/views/kelas/index.php
+++ app/views/kelas/index.php
@@ -5,7 +5,7 @@
     <?php foreach ($kelas as $kelas): ?>
         <div>
             
-            <p><?= htmlspecialchars($kelas['nama_kelas']) ?> - <?= htmlspecialchars($kelas['waktu']) ?> - <?= htmlspecialchars($kelas['peltih']) ?> - <?= htmlspecialchars($kelas['kuota']) ?>
+            <p><?= htmlspecialchars($kelas['nama_kelas']) ?> - <?= htmlspecialchars($kelas['waktu']) ?> - <?= htmlspecialchars($kelas['pelatih']) ?> - <?= htmlspecialchars($kelas['kuota']) ?>
             
             <a href="/kelas/edit/<?php echo $kelas['id_kelas']; ?>">Edit</a> |
             <a href="/kelas/delete/<?php echo $kelas['id_kelas']; ?>" onclick="return confirm('Are you sure?')">Delete</a>
